ee87d966521604304670d278e1d5d77bbdd876c3
saiad <saiad@aditya>
2022-03-23 13:01:28 +0530

    commit

---
+++ java-basic/src/assignment2/Area.java
@@ -0,0 +1,6 @@
+package assignment2;
+
+public interface Area {
+	public double getArea();
+
+}
---
+++ java-basic/src/assignment2/Shape.java
@@ -0,0 +1,13 @@
+package assignment2;
+
+public class Shape {
+String n;
+	
+	Shape(){
+	}
+	
+	public String getName() {
+		return n;
+	}
+
+}
